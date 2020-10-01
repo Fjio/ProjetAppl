eb2260d7c752ff3518c08bc59c188131c9c536a3
Pierre Bourreau <pibourreau@laposte.net>
2020-10-01 15:11:57 +0200

    Responsiveness added to base twig

--- src/Controller/MailerController.php
+++ src/Controller/MailerController.php
@@ -1,9 +1,12 @@
-// src/Controller/MailerController.php
+<?php
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
+
 
 class MailerController extends AbstractController
 {
