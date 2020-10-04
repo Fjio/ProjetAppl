cd0a268333289c1e58a5dab1784ac64bff9cfe45
Pierre Bourreau <pibourreau@laposte.net>
2020-10-04 17:44:57 +0200

    working register page

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -3,9 +3,11 @@
 // app/src/Controller/RegistrationController.php
 namespace App\Controller;
 
-use App\Entity\Utilisateur as User;
+use App\Entity\Utilisateur;
+use App\Entity\Personne;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
@@ -17,6 +19,12 @@ use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class RegistrationController extends AbstractController
 {
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
     /**
      * Undocumented function
      * @Route("/inscription", name="app_inscription")
@@ -38,16 +46,27 @@ class RegistrationController extends AbstractController
             ))
             ->getForm();
 
+        
         $form->handleRequest($request);
+        // no unique username check ! warning
         if ($form->isSubmitted() && $form->isValid()) {
             // Met à jour la candidature avec les notes
             $data = $form->getData();
-            //$candidatureEleve -> setNoteAutobiographie($data['noteAutobiographie']);
-
-            // Sauvegarde la candidature
-            //$entityManager = $this->getDoctrine()->getManager();
-            //$entityManager->persist($candidatureEleve);
-            //$entityManager->flush();
+            //manager import
+            $manager = $this->getDoctrine()->getManager();
+            //new personne defined
+            $elevePersonne = new Personne();
+            $elevePersonne -> setMail($data['username']);
+            $manager -> persist($elevePersonne);
+            //new user defined
+            $eleveUser = new Utilisateur();
+            $eleveUser -> setRoles("ROLE_ELEVE");
+            $eleveUser -> setIdPersonne($elevePersonne);
+            $eleveUser -> setUsername($data['username']); 
+            $eleveUser -> setPassword($this->passwordEncoder->encodePassword($eleveUser,$data['plainPassword']));
+            $manager -> persist($eleveUser);
+            //flush it
+            $manager->flush();
             return $this->redirectToRoute('app_login');
         }
         return $this->render('inscription/index.html.twig', [
